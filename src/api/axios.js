e55a2977a1d607675e22cd9ab10c16b89546545b
Darshan Mangukiya <darshanmangukiya95@gmail.com>
2026-05-27 17:14:16 +0530

    Update baseURL to use environment variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:8000/api",
-  baseURL: "https://habittracker-backend-5tbl.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
